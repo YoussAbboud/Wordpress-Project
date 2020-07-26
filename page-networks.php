03c940b46e715c23c248ecb750980854fac3ac0c
YoussAbboud <youssef.abboud01@lau.edu>
2020-07-26 16:33:33 +0300

    updated the archives

---
+++ page-networks.php
@@ -0,0 +1,32 @@
+<?php get_header();?>
+
+<div class="container">
+<div class="row">
+<div class="col-12">
+
+<?php 
+// wp-query to get all published posts without pagination
+$allPostsWPQuery = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
+ 
+<?php if ( $allPostsWPQuery->have_posts() ) : ?>
+ 
+<ul>
+     <?php while ( $allPostsWPQuery->have_posts() ) : $allPostsWPQuery->the_post(); ?>
+        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <?php endwhile; ?>
+</ul>
+     <?php wp_reset_postdata(); ?>
+ <?php else : ?>
+    <p><?php _e( 'There no posts to display.' ); ?></p>
+<?php endif; ?>
+
+
+
+</div>
+</div>
+</div>
+
+
+
+
+<?php get_footer();?>
